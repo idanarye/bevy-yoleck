f17a1b6768bacec1618ce98b785ee13a6ba5eafa
IdanArye <idanarye@gmail.com>
2022-04-25 23:25:50 +0300

    Change the design to "recreated" entities from the data

--- examples/example.rs
+++ examples/example.rs
@@ -1,5 +1,7 @@
+use bevy::ecs::system::EntityCommands;
 use bevy::prelude::*;
-use bevy_egui::{EguiPlugin, EguiContext, egui};
+use bevy::utils::HashMap;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum AppMode {
@@ -13,11 +15,10 @@ fn main() {
     app.add_plugin(EguiPlugin);
     app.add_startup_system(setup_camera);
     app.add_startup_system(setup_entities); // TODO: replace with entity setup from data;
-    app.add_startup_system(setup_yoleck_editing);
-    app.add_system_set(SystemSet::on_update(AppMode::Editor).with_system(yoleck_editor.exclusive_system()));
+    app.add_system_set(SystemSet::on_update(AppMode::Editor).with_system(yoleck_editor));
     app.insert_resource(YoleckState {
         entity_being_edited: None,
-        edit_functions: Default::default(),
+        entities: Default::default(),
     });
     app.run();
 }
@@ -26,71 +27,95 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
-fn setup_entities(mut commands: Commands) {
-    commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: Color::RED,
-                custom_size: Some(Vec2::new(20.0, 20.0)),
-                ..Default::default()
-            },
-            transform: Transform::from_xyz(-50.0, 0.0, 0.0),
-            ..Default::default()
-        })
-        .insert(YoleckOwned);
-
-    commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: Color::BLUE,
-                custom_size: Some(Vec2::new(20.0, 20.0)),
-                ..Default::default()
-            },
-            transform: Transform::from_xyz(50.0, 0.0, 0.0),
-            ..Default::default()
-        })
-        .insert(YoleckOwned);
+fn setup_entities(mut yoleck: ResMut<YoleckState>, mut commands: Commands) {
+    for example_box in [
+        ExampleBox {
+            position: Vec2::new(0.0, -50.0),
+            color: Color::RED,
+        },
+        ExampleBox {
+            position: Vec2::new(0.0, 50.0),
+            color: Color::BLUE,
+        },
+    ] {
+        let mut cmd = commands.spawn();
+        cmd.insert(YoleckOwned);
+        example_box.populate(&mut cmd);
+        yoleck.entities.insert(cmd.id(), example_box);
+    }
 }
 
 #[derive(Component)]
 pub struct YoleckOwned;
 
-fn yoleck_editor(world: &mut World) {
-    let mut egui_context = world.resource_mut::<EguiContext>().clone();
-    let egui_ctx = egui_context.ctx_mut();
-    egui::Window::new("Level Editor").show(egui_ctx, |ui| {
-        // let query_state = world.query::<EditBox>();
-        world.resource_scope(|world, mut yoleck: Mut<YoleckState>| {
-            for (entity, _) in world.query::<(Entity, &YoleckOwned)>().iter(world) {
-                ui.selectable_value(&mut yoleck.entity_being_edited, Some(entity), format!("{:?}", entity));
+struct YoleckState {
+    entity_being_edited: Option<Entity>,
+    entities: HashMap<Entity, ExampleBox>,
+}
+
+fn yoleck_editor(
+    mut egui_context: ResMut<EguiContext>,
+    mut yoleck: ResMut<YoleckState>,
+    mut commands: Commands,
+) {
+    egui::Window::new("Level Editor").show(egui_context.ctx_mut(), |ui| {
+        let yoleck = yoleck.as_mut();
+        for entity in yoleck.entities.keys() {
+            ui.selectable_value(
+                &mut yoleck.entity_being_edited,
+                Some(*entity),
+                format!("{:?}", entity),
+            );
+        }
+        if let Some(entity) = yoleck.entity_being_edited {
+            if let Some(data) = yoleck.entities.get_mut(&entity) {
+                data.edit(ui);
+                data.populate(&mut commands.entity(entity));
             }
-            if let Some(entity) = yoleck.entity_being_edited {
-                for edit_function in yoleck.edit_functions.iter_mut() {
-                    edit_function(ui, world, entity);
-                }
-            }
-        });
+        }
     });
 }
 
-fn edit_box() -> impl Send + Sync + FnMut(&mut egui::Ui, &mut World, Entity) {
-    let mut query_state = None;
-    move |ui, world, entity| {
-        let query_state = query_state.get_or_insert_with(|| world.query::<(&mut Transform,)>());
-        if let Ok((transform,)) = query_state.get_mut(world, entity) {
-            let transform = transform.into_inner();
-            ui.add(egui::Slider::new(&mut transform.translation.y, -100.0..=100.0));
+struct ExampleBox {
+    position: Vec2,
+    color: Color,
+}
+
+impl ExampleBox {
+    fn populate(&self, cmd: &mut EntityCommands) {
+        cmd.insert_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: self.color,
+                custom_size: Some(Vec2::new(20.0, 20.0)),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(self.position.extend(0.0)),
+            ..Default::default()
+        });
+    }
+
+    fn edit(&mut self, ui: &mut egui::Ui) {
+        ui.add(egui::Slider::new(&mut self.position.x, -100.0..=100.0).text("X Position"));
+        self.color = self.color.as_rgba();
+        if let Color::Rgba {
+            red,
+            green,
+            blue,
+            alpha,
+        } = &mut self.color
+        {
+            let mut color32: egui::Color32 =
+                egui::Rgba::from_rgba_unmultiplied(*red, *green, *blue, *alpha).into();
+            egui::widgets::color_picker::color_picker_color32(
+                ui,
+                &mut color32,
+                egui::color_picker::Alpha::OnlyBlend,
+            );
+            let rgba: egui::Rgba = color32.into();
+            *red = rgba.r();
+            *green = rgba.g();
+            *blue = rgba.b();
+            *alpha = rgba.a();
         }
     }
-}
-
-type YoleckEditFunction = Box<dyn Send + Sync + FnMut(&mut egui::Ui, &mut World, Entity)>;
-
-struct YoleckState {
-    entity_being_edited: Option<Entity>,
-    edit_functions: Vec<YoleckEditFunction>,
-}
-
-fn setup_yoleck_editing(mut yoleck: ResMut<YoleckState>) {
-    yoleck.edit_functions.push(Box::new(edit_box()));
 }
