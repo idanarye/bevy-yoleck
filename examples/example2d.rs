fe8a737d0cc80300f484ddec81f3b99fe127fad0
Idan Arye <IdanArye@gmail.com>
2022-05-24 16:37:08 +0300

    Start converting example2d to a (very simple) game

--- examples/example2d.rs
+++ examples/example2d.rs
@@ -1,20 +1,20 @@
 use std::path::Path;
 
 use bevy::prelude::*;
-use bevy::sprite::Anchor;
 use bevy_egui::{egui, EguiPlugin};
 
-use bevy_yoleck::tools_2d::position_edit_adapter;
+use bevy_yoleck::tools_2d::{position_edit_adapter, Transform2dProjection};
 use bevy_yoleck::{
-    YoleckEdit, YoleckEditorState, YoleckExtForApp, YoleckLoadingCommand, YoleckPluginForEditor,
-    YoleckPluginForGame, YoleckPopulate, YoleckTypeHandlerFor,
+    YoleckEdit, YoleckEditorLevelsDirectoryPath, YoleckEditorState, YoleckExtForApp,
+    YoleckLoadingCommand, YoleckPluginForEditor, YoleckPluginForGame, YoleckPopulate,
+    YoleckTypeHandlerFor,
 };
 use serde::{Deserialize, Serialize};
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
-    app.init_resource::<FontAsset>();
+    app.init_resource::<GameAssets>();
     let level = std::env::args().nth(1);
     if let Some(level) = level {
         app.add_plugin(YoleckPluginForGame);
@@ -29,27 +29,27 @@ fn main() {
     } else {
         app.add_plugin(EguiPlugin);
         app.add_plugin(YoleckPluginForEditor);
+        // Adding `YoleckEditorLevelsDirectoryPath` is not usually required -
+        // `YoleckPluginForEditor` will add one with "assets/levels". Here we want to support
+        // example3d in the same repository so we use different directories.
+        app.insert_resource(YoleckEditorLevelsDirectoryPath(
+            Path::new(".").join("assets").join("levels2d"),
+        ));
         app.add_plugin(bevy_yoleck::tools_2d::YoleckTools2dPlugin);
     }
+
+    app.add_startup_system(setup_camera);
+
     app.add_yoleck_handler({
-        YoleckTypeHandlerFor::<ExampleBox>::new("ExampleBox")
-            .populate_with(populate_box)
-            .with(position_edit_adapter(|example_box: &mut ExampleBox| {
-                bevy_yoleck::tools_2d::Transform2dProjection {
-                    translation: &mut example_box.position,
-                }
-            }))
-            .edit_with(edit_box)
-    });
-    app.add_yoleck_handler({
-        YoleckTypeHandlerFor::<ExampleBox2>::new("ExampleBox2")
-            .populate_with(populate_box2)
-            .with(position_edit_adapter(|example_box2: &mut ExampleBox2| {
-                bevy_yoleck::tools_2d::Transform2dProjection {
-                    translation: &mut example_box2.position,
+        YoleckTypeHandlerFor::<Player>::new("Player")
+            .populate_with(populate_player)
+            .with(position_edit_adapter(|data: &mut Player| {
+                Transform2dProjection {
+                    translation: &mut data.position,
                 }
             }))
     });
+
     app.add_yoleck_handler({
         YoleckTypeHandlerFor::<FloatingText>::new("FloatingText")
             .populate_with(populate_text)
@@ -60,14 +60,10 @@ fn main() {
             }))
             .edit_with(edit_text)
     });
-    app.add_startup_system(setup_camera);
-    if true {
-        app.add_system(move_the_boxes);
-    } else {
-        app.add_system_set(
-            SystemSet::on_update(YoleckEditorState::GameActive).with_system(move_the_boxes),
-        );
-    }
+
+    app.add_system_set(
+        SystemSet::on_update(YoleckEditorState::GameActive).with_system(control_player),
+    );
     app.run();
 }
 
@@ -77,100 +73,68 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(camera);
 }
 
-struct FontAsset(Handle<Font>);
+struct GameAssets {
+    player_sprite: Handle<Image>,
+    #[allow(dead_code)]
+    font: Handle<Font>,
+}
 
-impl FromWorld for FontAsset {
+impl FromWorld for GameAssets {
     fn from_world(world: &mut World) -> Self {
-        Self(
-            world
-                .resource::<AssetServer>()
-                .load("fonts/FiraSans-Bold.ttf"),
-        )
+        let asset_server = world.resource::<AssetServer>();
+        Self {
+            player_sprite: asset_server.load("sprites/player.png"),
+            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        }
     }
 }
 
 #[derive(Component)]
-struct Velocity(Vec2);
-
-#[derive(Clone, PartialEq, Serialize, Deserialize)]
-struct ExampleBox {
-    #[serde(default)]
-    position: Vec2,
-    #[serde(default)]
-    z: f32,
-    #[serde(default)]
-    color: Color,
-}
-
-fn populate_box(mut populate: YoleckPopulate<ExampleBox>) {
-    populate.populate(|ctx, data, mut cmd| {
-        cmd.insert_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: data.color,
-                custom_size: Some(Vec2::new(20.0, 20.0)),
-                anchor: Anchor::BottomLeft,
-                ..Default::default()
-            },
-            transform: Transform::from_translation(data.position.extend(data.z)),
-            ..Default::default()
-        });
-        if !ctx.is_in_editor() {
-            cmd.insert(Velocity(Vec2::new(1.0, 0.0)));
-        }
-    });
-}
-
-fn edit_box(mut edit: YoleckEdit<ExampleBox>) {
-    edit.edit(|_ctx, data, ui| {
-        ui.add(egui::DragValue::new(&mut data.z).prefix("Z:"));
-        data.color = data.color.as_rgba();
-        if let Color::Rgba {
-            red,
-            green,
-            blue,
-            alpha,
-        } = &mut data.color
-        {
-            let color32: egui::Color32 =
-                egui::Rgba::from_rgba_unmultiplied(*red, *green, *blue, *alpha).into();
-            let mut rgba: egui::Rgba = color32.into();
-            egui::widgets::color_picker::color_edit_button_rgba(
-                ui,
-                &mut rgba,
-                egui::color_picker::Alpha::OnlyBlend,
-            );
-            *red = rgba.r();
-            *green = rgba.g();
-            *blue = rgba.b();
-            *alpha = rgba.a();
-        }
-    });
+struct PlayerControl {
+    speed: f32,
 }
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
-struct ExampleBox2 {
+struct Player {
     #[serde(default)]
     position: Vec2,
 }
 
-fn populate_box2(mut populate: YoleckPopulate<ExampleBox2>) {
+fn populate_player(mut populate: YoleckPopulate<Player>, assets: Res<GameAssets>) {
     populate.populate(|_ctx, data, mut cmd| {
         cmd.insert_bundle(SpriteBundle {
             sprite: Sprite {
-                color: Color::GREEN,
-                anchor: Anchor::TopRight,
-                custom_size: Some(Vec2::new(30.0, 30.0)),
+                custom_size: Some(Vec2::new(100.0, 100.0)),
                 ..Default::default()
             },
             transform: Transform::from_translation(data.position.extend(0.0)),
+            texture: assets.player_sprite.clone(),
             ..Default::default()
         });
+        cmd.insert(PlayerControl { speed: 400.0 });
     });
 }
 
-fn move_the_boxes(mut query: Query<(&mut Transform, &Velocity)>) {
-    for (mut transform, velocity) in query.iter_mut() {
-        transform.translation += velocity.0.extend(0.0);
+fn control_player(
+    mut player_query: Query<(&PlayerControl, &mut Transform)>,
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>,
+) {
+    let mut velocity = Vec3::ZERO;
+    if input.pressed(KeyCode::Up) {
+        velocity += Vec3::Y;
+    }
+    if input.pressed(KeyCode::Down) {
+        velocity -= Vec3::Y;
+    }
+    if input.pressed(KeyCode::Left) {
+        velocity -= Vec3::X;
+    }
+    if input.pressed(KeyCode::Right) {
+        velocity += Vec3::X;
+    }
+    for (player_control, mut player_transform) in player_query.iter_mut() {
+        player_transform.translation += velocity * player_control.speed * time.delta_seconds();
     }
 }
 
@@ -188,14 +152,14 @@ fn default_scale() -> f32 {
     1.0
 }
 
-fn populate_text(mut populate: YoleckPopulate<FloatingText>, font_asset: Res<FontAsset>) {
+fn populate_text(mut populate: YoleckPopulate<FloatingText>, assets: Res<GameAssets>) {
     populate.populate(|_ctx, data, mut cmd| {
         cmd.insert_bundle(Text2dBundle {
             text: {
                 Text::with_section(
                     data.text.clone(),
                     TextStyle {
-                        font: font_asset.0.clone(),
+                        font: assets.font.clone(),
                         font_size: 72.0,
                         color: Color::WHITE,
                     },
